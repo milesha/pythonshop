dbf85f1b75c6117c15cba06194199a487955492d
milesha <45200178+milesha@users.noreply.github.com>
2018-12-11 20:15:57 +0500

    Pythonshop develop v0.3

--- projectv1.py
+++ projectv1.py
@@ -3,7 +3,7 @@ from PyQt5 import uic, QtCore
 from PyQt5.QtWidgets import QApplication, QWidget, \
     QPushButton, QHBoxLayout
 from PyQt5.QtWidgets import QInputDialog, QFileDialog, QSlider
-from PyQt5.QtWidgets import QLabel, QLineEdit, QMainWindow
+from PyQt5.QtWidgets import QLabel, QLineEdit, QMainWindow, QAction, QMessageBox
 from PyQt5.QtGui import QPixmap, QIcon
 from PIL import Image, ImageDraw
 from PyQt5.QtCore import Qt
@@ -21,6 +21,19 @@ class MyWidget(QMainWindow):
         self.setWindowTitle('Pythonshop v1.0')
         self.setWindowIcon(QIcon('icon.png'))
         self.name = None
+        #Флаг для корректного сохранения
+        self.f = True
+
+        #Для обработки яркости
+        self.light = 0
+        self.copies = []
+
+        #Обработка закрытия программы
+        quit = QAction("Quit", self)
+        quit.triggered.connect(self.closeEvent)
+        menubar = self.menuBar()
+        fmenu = menubar.addMenu("File")
+        fmenu.addAction(quit)
 
         #Устанавливаем цвета кнопок
         self.beginButton.setStyleSheet(
@@ -44,11 +57,11 @@ class MyWidget(QMainWindow):
         self.transfLR.clicked.connect(self.transferfLeftRight)
         self.transUD.clicked.connect(self.transferfUpDown)
 
-        self.brightness.setMinimum(-100)
-        self.brightness.setMaximum(100)
-        self.brightness.setValue(-100)
+        self.brightness.setMinimum(0)
+        self.brightness.setMaximum(4)
+        self.brightness.setValue(0)
         self.brightness.setTickPosition(QSlider.TicksBelow)
-        self.brightness.setTickInterval(50)
+        self.brightness.setTickInterval(2)
         self.brightness.valueChanged.connect(self.valueChangeBr)
 
         self.noise.setMinimum(0)
@@ -63,55 +76,18 @@ class MyWidget(QMainWindow):
         self.saturation.setTickPosition(QSlider.TicksBelow)
         self.saturation.setTickInterval(50)
 
+        #Фильтры
         self.filtrBlack.clicked.connect(self.blackFiltr)
         self.filtrOriginal.clicked.connect(self.originalFiltr)
         self.filtrRetro.clicked.connect(self.retroFiltr)
         self.filtrNegativ.clicked.connect(self.negativFiltr)
 
-    #Слайдер для яркости изображения
-    def valueChangeBr(self):
-        k = self.brightness.value()
-        image = Image.open(self.name)
-        draw = ImageDraw.Draw(image)
-        width = image.size[0]
-        height = image.size[1]
-        pix = image.load()
-        for i in range(width):
-            for j in range(height):
-                a = pix[i, j][0] + k
-                b = pix[i, j][1] + k
-                c = pix[i, j][2] + k
-                if a < 0:
-                    a = 0
-                if b < 0:
-                    b = 0
-                if c < 0:
-                    c = 0
-                if a > 255:
-                    a = 255
-                if b > 255:
-                    b = 255
-                if c > 255:
-                    c = 255
-                draw.point((i, j), (a, b, c))
-        image.save(self.name)
-        pixmap = QPixmap(self.name)
-        pixmap = pixmap.scaled(511, 331, QtCore.Qt.KeepAspectRatio)
-        self.picture.setPixmap(pixmap)
-
-    #Обработка нажатий клавиш
-    def keyPressEvent(self, event):
-        if event.key() == Qt.Key_A:
-            self.rotateL()
-        if event.key() == Qt.Key_D:
-            self.rotateR()
-
-    #Открываем картинку
+    # Открываем картинку
     def openFileNameDialog(self):
         options = QFileDialog.Options()
         options |= QFileDialog.DontUseNativeDialog
         self.name, _ = QFileDialog.getOpenFileName(self, "QFileDialog.getOpenFileName()", "",
-                                                  "All Files (*);;Python Files (*.py)", options=options)
+                                                   "All Files (*);;Python Files (*.py)", options=options)
         if self.name:
             self.nameCopy = self.name
             image = Image.open(self.name)
@@ -122,6 +98,50 @@ class MyWidget(QMainWindow):
             pixmap = QPixmap(self.name)
             pixmap = pixmap.scaled(511, 331, QtCore.Qt.KeepAspectRatio)
             self.picture.setPixmap(pixmap)
+
+    #Обработка закрытия программы
+    def closeEvent(self, event):
+        close = QMessageBox()
+        close.setText("Сохранить действия в файле?")
+        close.setWindowIcon(QIcon('icon.png'))
+        close.setStandardButtons(QMessageBox.Yes | QMessageBox.Cancel)
+        close = close.exec()
+
+        if close == QMessageBox.Yes:
+            if self.f == False:
+                event.accept()
+            elif self.name:
+                im = Image.open(self.name)
+                im.save(self.nameCopy)
+                os.remove(self.name)
+                event.accept()
+        else:
+            if self.name:
+                os.remove(self.name)
+
+    #Слайдер для яркости изображения
+    def valueChangeBr(self):
+        if self.light == 0:
+            image = np.asarray(Image.open(self.name))
+            self.copies.append(image)
+            for i in range(4):
+                image1 = self.copies[i] // 1.5
+                self.copies.append(image1)
+            self.light = 1
+            Image.fromarray(np.uint8(self.copies[self.brightness.value()])).save(self.name)
+        else:
+            Image.fromarray(np.uint8(self.copies[self.brightness.value()])).save(self.name)
+
+        pixmap = QPixmap(self.name)
+        pixmap = pixmap.scaled(511, 331, QtCore.Qt.KeepAspectRatio)
+        self.picture.setPixmap(pixmap)
+
+    #Обработка нажатий клавиш
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_A:
+            self.rotateL()
+        if event.key() == Qt.Key_D:
+            self.rotateR()
 
     #Черно-белый фильтр
     def blackFiltr(self):
@@ -140,10 +160,11 @@ class MyWidget(QMainWindow):
             self.picture.setPixmap(pixmap)
 
 
-    #Возвращение оригинального цвета фото
+    #Возвращение оригинального фото
     def originalFiltr(self):
         if self.name:
             if self.original:
+                self.brightness.setValue(0)
                 self.original.save(self.name)
                 pixmap = QPixmap(self.name)
                 pixmap = pixmap.scaled(511, 331, QtCore.Qt.KeepAspectRatio)
@@ -183,19 +204,8 @@ class MyWidget(QMainWindow):
     #Фильтр Негатив
     def negativFiltr(self):
         if self.name:
-            image = Image.open(self.name)
-            draw = ImageDraw.Draw(image)
-            width = image.size[0]
-            height = image.size[1]
-            pix = image.load()
-            for i in range(width):
-                for j in range(height):
-                    a = pix[i, j][0]
-                    b = pix[i, j][1]
-                    c = pix[i, j][2]
-                    draw.point((i, j), (255 - a, 255 - b, 255 - c))
-            del draw
-            image.save(self.name)
+            image = np.asarray(Image.open(self.name))
+            Image.fromarray(np.uint8(image * -1)).save(self.name)
             pixmap = QPixmap(self.name)
             pixmap = pixmap.scaled(511, 331, QtCore.Qt.KeepAspectRatio)
             self.picture.setPixmap(pixmap)
@@ -244,7 +254,7 @@ class MyWidget(QMainWindow):
     #Сохранение результата
     def savePic(self):
         if self.name:
-
+            self.f = False
             im = Image.open(self.name)
             im.save(self.nameCopy)
             os.remove(self.name)
